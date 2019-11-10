4dfee30e39266c7ad9a896b53d2611f6bbbb0a3c
ljerome <ljerome@student.ethz.ch>
2019-11-10 16:03:17 +0100

    Initial script w/ logictic regression
    Definition of many additional basic features

---
+++ src/utilities/utility_functions.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 2."""
+import csv
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import os,sys
+from PIL import Image
+
+
+# Helper functions
+
+def load_image(infilename):
+    data = mpimg.imread(infilename)
+    return data
+
+def img_float_to_uint8(img):
+    rimg = img - np.min(img)
+    rimg = (rimg / np.max(rimg) * 255).round().astype(np.uint8)
+    return rimg
+
+# Concatenate an image and its groundtruth
+def concatenate_images(img, gt_img):
+    nChannels = len(gt_img.shape)
+    w = gt_img.shape[0]
+    h = gt_img.shape[1]
+    if nChannels == 3:
+        cimg = np.concatenate((img, gt_img), axis=1)
+    else:
+        gt_img_3c = np.zeros((w, h, 3), dtype=np.uint8)
+        gt_img8 = img_float_to_uint8(gt_img)          
+        gt_img_3c[:,:,0] = gt_img8
+        gt_img_3c[:,:,1] = gt_img8
+        gt_img_3c[:,:,2] = gt_img8
+        img8 = img_float_to_uint8(img)
+        cimg = np.concatenate((img8, gt_img_3c), axis=1)
+    return cimg
+
+def img_crop(im, w, h):
+    list_patches = []
+    imgwidth = im.shape[0]
+    imgheight = im.shape[1]
+    is_2d = len(im.shape) < 3
+    for i in range(0,imgheight,h):
+        for j in range(0,imgwidth,w):
+            if is_2d:
+                im_patch = im[j:j+w, i:i+h]
+            else:
+                im_patch = im[j:j+w, i:i+h, :]
+            list_patches.append(im_patch)
+    return list_patches
+
+#DEFINITION OF FEATURES
+# Extract 6-dimensional features consisting of average RGB color as well as variance
+def extract_features(img):
+    #shape 1x3 + 1x3 = 1x6
+    feat_m = np.mean(img, axis=(0,1))
+    feat_v = np.var(img, axis=(0,1))
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+# Extract 2-dimensional features consisting of average gray color as well as variance
+def extract_features_2d(img):
+    feat_m = np.mean(img)
+    feat_v = np.var(img)
+    feat = np.append(feat_m, feat_v)
+    return feat
+
+# Extract features for a given image
+def extract_img_features(filename,patch_size):
+    img = load_image(filename)
+    img_patches = img_crop(img, patch_size, patch_size)
+    X = np.asarray([ extract_features_2d(img_patches[i]) for i in range(len(img_patches))])
+    return X
