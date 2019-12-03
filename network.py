eaaa885ae32863a05ffbe947a5e9c9199fd5d6e4
LucLeGros <3gmichels@gmail.com>
2019-12-03 15:02:26 +0100

    easier UNet creation

--- network.py
+++ network.py
@@ -140,7 +140,7 @@ import torch.nn.functional as F
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True):
+    def __init__(self, n_channels=3, n_classes=2, bilinear=True):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
